dac69ea57ea421fbea9acd8516089c586d8d860f
Hrvoje <htomic4@gmail.com>
2023-04-23 16:29:54 +0100

    commit

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import Stripe from "stripe";
+
+const prisma = new PrismaClient();
+
+export default NextAuth({
+    adapter: PrismaAdapter(prisma),
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }),
+    ],
+    events: {
+      createUser: async({user}) => {
+        const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+          apiVersion:"2022-11-15",
+        })
+        //create stripe cusotmer
+        if (user.name && user.email) {
+          const customer = await stripe.customers.create({
+            email: user.email,
+            name: user.name
+          })
+          //update prisma with stripecustoemrId
+          await prisma.user.update({
+            where: {id: user.id},
+            data: {stripeCustomerId: customer.id}
+          })
+        }
+      }
+    }
+  });
+  
